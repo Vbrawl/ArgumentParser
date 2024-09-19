212404ee532ebd18eb4065028ea4142970537e2f
VBrawl <konstantosjim@gmail.com>
2024-09-19 23:09:05 +0300

    Added missing unit test

--- tests/unit_test/argument_component.c
+++ tests/unit_test/argument_component.c
@@ -1,5 +1,31 @@
-
+#include <ObjectWrapper/object.h>
+#include <ObjectWrapper/ow_string.h>
+#include "argument_parser.h"
 
 int main() {
+
+  OWO_String_t* fullname = OWString_ConstructSimple("alpha");
+  OWO_String_t* shortname = OWString_ConstructSimple("a");
+  bool accepts_value = true;
+
+  O_ArgumentComponent_t* component = ArgumentComponent_Construct(fullname, shortname, accepts_value);
+
+  OWO_String_t* fullname2 = ArgumentComponent_FullName(component);
+  OWO_String_t* shortname2 = ArgumentComponent_ShortName(component);
+  bool accepts_value2 = ArgumentComponent_AcceptsValue(component);
+
+  if(OWString_CompareOWString(fullname, fullname2) != 0) return 1;
+  if(OWString_CompareOWString(shortname, shortname2) != 0) return 2;
+  if(accepts_value != accepts_value2) return 3;
+
+  OWObject_UnRef(fullname2);
+  OWObject_UnRef(shortname2);
+
+  OWObject_UnRef(component);
+
+  OWObject_UnRef(fullname);
+  OWObject_UnRef(shortname);
+
+
   return 0;
 }
