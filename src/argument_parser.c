0eedf9c3fe6a0531a39e953212316d17e960ce1d
VBrawl <konstantosjim@gmail.com>
2024-09-20 10:25:07 +0300

    Removed printf statements

--- src/argument_parser.c
+++ src/argument_parser.c
@@ -81,8 +81,6 @@ const char* __Helper__ArgumentParser_ProcessValue(O_ArgumentParser_t* this, bool
   return val;
 }
 
-#include <stdio.h>
-
 int ArgumentParser_Process(O_ArgumentParser_t* this, OWO_String_t* argname, OWO_String_t* argval) {
   OWString_SetSimple(argname, "");
   OWString_SetSimple(argval, "");
@@ -90,7 +88,6 @@ int ArgumentParser_Process(O_ArgumentParser_t* this, OWO_String_t* argname, OWO_
   ArgumentParser_t* const obj = OWObject_FindObjectInClass(this, OWID_ArgumentParser_t);
   if(obj == NULL) return -1;
 
-  printf("current_index: %d, argc: %d\n", obj->current_index, obj->argc);
   if(obj->current_index >= obj->argc
      || obj->current_sub_index + 1 >= strlen(obj->argv[obj->current_index])) {
     return 1;
@@ -129,7 +126,6 @@ int ArgumentParser_Process(O_ArgumentParser_t* this, OWO_String_t* argname, OWO_
 
   if(is_short_name && !value_found) {
     obj->current_sub_index += 1;
-    printf("current_sub_index: %d, strlen: %ld\n", obj->current_sub_index, strlen(obj->argv[obj->current_index]));
     if(obj->current_sub_index >= strlen(obj->argv[obj->current_index])) {
       obj->current_sub_index = 0;
       obj->current_index += 1;
